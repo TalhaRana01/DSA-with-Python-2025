50ab83dc71038f4253a88e61ba42e5927008d600
Talha Rana <mtalharana093@gmail.com>
2025-11-09 10:26:19 +0500

    basic python practice

---
+++ lecture_01.py
@@ -0,0 +1,40 @@
+
+
+name = "talha Rana" # string value
+age = 33            # int number value
+heighht = 5.6       # float number value
+
+
+# print(name, age, heighht)
+
+
+## Taking value from users with input function
+# user_name = input("Enter your name ")
+
+
+## Display value result with output function
+# print(f"My name is {user_name}")
+
+
+#  Input functions take user value 
+# first_name = input("Enter your name")
+# email = input("Enter your email")
+# age = int(input("Enter your age"))
+# height = float(input("Enter your height"))
+
+# Display user value
+# print(f"My Name is {first_name}, email is {email}, and I'm {age} years old, and total height is {height} ")
+
+
+num1 = int(input("Enter num 1 :"))
+num2 = int(input("Enter num  2 :"))
+
+
+print(f"\nTotal sum of {num1} and {num2} is = {num1 + num2}")
+print(f"\nTotal sum of {num1} and {num2} is = {num1 - num2}")
+print(f"\nTotal sum of {num1} and {num2} is = {num1 / num2}")
+print(f"\nTotal sum of {num1} and {num2} is = {num1 % num2}")
+print(f"\nTotal sum of {num1} and {num2} is = {num1 * num2}")
+print(f"\nTotal sum of {num1} and {num2} is = {num1 ** num2}")
+
+
